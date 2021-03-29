bf56f83307cb68b3c2f67be84b61f61710383e85
Muhammad <muhammad.gaamal@gmail.com>
2021-03-29 12:25:13 +0200

    NPM Scripts

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$("#carouselButton").click(function(){
+    if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+        $("#mycarousel").carousel('pause');
+        $("#carouselButton").children("span").removeClass('fa-pause');
+        $("#carouselButton").children("span").addClass('fa-play');
+    }
+    else if ($("#carouselButton").children("span").hasClass('fa-play')){
+        $("#mycarousel").carousel('cycle');
+        $("#carouselButton").children("span").removeClass('fa-play');
+        $("#carouselButton").children("span").addClass('fa-pause');                    
+    }
+});
+
+$('#myLogin').on('click', function(ev) {
+    $('#loginModal').modal('show');
+});
+
+$('#myReverse').on('click', function(ev) {
+    $('#reserveModal').modal('show');
+});
